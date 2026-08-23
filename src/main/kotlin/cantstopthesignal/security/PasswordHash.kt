fdb0638a6719868eeb92f9042633762c12cd6941
dustyn <dustyn_gibb@hotmail.com>
2026-08-23 06:56:41 +0000

    Lower the gensalt hashing rounds to speed up login a bit

--- src/main/kotlin/cantstopthesignal/security/PasswordHash.kt
+++ src/main/kotlin/cantstopthesignal/security/PasswordHash.kt
@@ -4,5 +4,5 @@ import org.mindrot.jbcrypt.BCrypt
 
 
 fun hashPassword(password: String): String {
-   return BCrypt.hashpw(password, BCrypt.gensalt(16))
+   return BCrypt.hashpw(password, BCrypt.gensalt(12))
 }
